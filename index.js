22cd90f840e88d453b85be5a47739fe6107182f4
Tommaso Cari Gallingani <tommycarigallingani@gmail.com>
2020-11-24 21:08:16 +0000

    primo programma in JS

---
+++ index.js
@@ -0,0 +1,18 @@
+let user = "Tommaso";
+let message = "Questo è il mio primo programma in JS!";
+
+alert(`Mi chiamo, ${user}!`)
+alert(message)
+
+let age = prompt("Quanti anni hai?", 100);
+alert(`Hai ${age} anni!`)
+
+let ready = confirm("Sei pronto per vedere le mie SKILL di programmatore???")
+
+alert("Ora ti mostrerò una semplice operazione...")
+let x = 7, y = 2;
+alert(y * x)
+alert("ecco fatto!")
+// dimostrare i calcoli matematici
+
+alert("Grazie per aver usato il mio primo programma in JavaScript, spero non ti abbia fatto schifo!")
